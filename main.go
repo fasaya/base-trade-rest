6226a59440090312e62aeaff8ac4462d294bbe16
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2024-06-25 14:28:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"base-trade-rest/database"
 )
 
-var PORT = ":3000"
+// var PORT = ":3000"
 
 func main() {
 	database.StartDB()
 
 	app := api.SetupRouter()
-	app.Run(PORT)
+	// app.Run(PORT)
+	app.Run()
 }
